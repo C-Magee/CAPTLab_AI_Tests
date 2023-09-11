778385ac7d24fa4816175c8880a2c50fca35fc2d
Corin-M <cmagee1212@gmail.com>
2023-09-11 13:49:28 -0700

    secret_1.py changed to keys.py
    I changed the name of the document holding the keys, and increased commenting.
    
    I additionally updated the knowledge base to hold a quick pdf describing the CAPT lab (Source: https://www.paloaltou.edu/graduate-programs/phd-programs/phd-clinical-psychology/research-group-directory/new-technologies-psy) to test the new knowledge base.

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from langchain.chat_models import ChatOpenAI
 import gradio as gr
 import os
 import streamlit as st
-from secrets_1 import openai_key
+#You will need to create a file called keys.py to hold your personal open AI Key using the following variable declaration
+# openai_key = "[YOUR OPENAI API KEY]"
+from keys import openai_key
 
+#Or you can directly input your Key it into the enviornmental variable declaration below 
+# in place of openai_key if you aren't intending to make your code public.
 os.environ["OPENAI_API_KEY"] = openai_key
 
+#create a json file holding the chat history including the prompts, knowledge, base and responses. 
 def construct_index(directory_path):
     max_input_size = 4096
     num_outputs = 512
@@ -25,15 +30,19 @@ def construct_index(directory_path):
 
     return index
 
+#cosntructs the chatbot response by querying the prompt formulated by the user and the json modifications
 def chatbot(input_text):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
     response = index.query(input_text, response_mode="compact")
     return response.response
 
+#Creates a basic user interface using gradio
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.components.Textbox(lines=7, label="Enter your text"),
                      outputs="text",
                      title="Custom-trained AI Chatbot")
 
+#Pulls the knowledge base from the docs folder
 index = construct_index("docs")
+#launches the AI on a local server. 
 iface.launch(share=True)
